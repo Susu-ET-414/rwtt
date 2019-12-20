f625161445ae86828cacce0104917b3217386f9e
Vlad satonin <vlad.satonin74@gmail.com>
2019-12-20 22:17:05 +0500

    Спринт 1 - 1,2,3,4,5

---
+++ revenge with a taste of tears/revenge with a taste of tears.cpp
@@ -0,0 +1,21 @@
+﻿#include "SFML/Graphics.hpp"
+#include <iostream>
+#include "fstream"
+#include "conio.h"
+#include "windows.h"
+#include "string.h"
+#include <cstdlib>
+#include <ctime>
+#include <sstream>
+#include "vector"
+
+int main(){
+	setlocale(LC_ALL, "Rus");
+
+	srand(time(NULL));
+	HWND hWnd = GetConsoleWindow();
+	//ShowWindow(hWnd, SW_HIDE);
+	ShowWindow(hWnd, SW_SHOW);
+
+	
+}
